df7bd9d333108b3a4ec12c7cd601c85b30c4d8bd
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-06-03 15:56:33 -0300

    Promesas

--- 01-typescript-intro/src/bases/03.classes.ts
+++ 01-typescript-intro/src/bases/03.classes.ts
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export class Pokemon {
 
     get imageUrl():string {
@@ -13,8 +15,16 @@ export class Pokemon {
     speak(){
        return 'Pikachu!'
     }
+
+    async getMoves(){
+        const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
+        const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.id}`)
+        console.log(data)
+        return resp.data.moves
+    }
 }
 
 
 export const pikachu = new Pokemon(25, 'Pikachu');
 console.log(pikachu)
+console.log(pikachu.getMoves())
