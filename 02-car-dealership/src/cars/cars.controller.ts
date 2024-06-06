8bb2a1d0717d588b64e888421faa52ea84987f87
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-06-06 11:52:11 -0300

    Primera peticion GET

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-    
+
+    private cars = ['Toyota', 'Ford', 'Chevrolet', 'Nissan', 'Honda', 'Hyundai'];
+
     @Get()
     getAllCars() {
-        return 'All cars';
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id) {
+        return this.cars[id]
     }
 }
