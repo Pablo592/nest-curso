e4c419e52f20d95f9df033d94f89f52d4945e645
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-06-12 10:53:09 -0300

    Dandole atributos a las columnas de las tablas

--- 03-teslo-shop/src/products/entities/product.entity.ts
+++ 03-teslo-shop/src/products/entities/product.entity.ts
@@ -1,3 +1,5 @@
+import { type } from "os";
+import { text } from "stream/consumers";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
@@ -10,4 +12,40 @@ export class Product {
         unique: true,
     })
     title: string
+
+    @Column('numeric', {
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string
+
+    @Column({
+        type: 'text',
+        unique: true
+    })
+    slug: string
+
+
+    @Column({
+        type: 'int',
+        default: 0
+    })
+    stock: number
+
+
+    @Column({
+        type: 'text',
+        array: true
+    })
+    sizes: string[]
+
+    @Column({
+        type: 'text'
+    })
+    gender: string
 }
