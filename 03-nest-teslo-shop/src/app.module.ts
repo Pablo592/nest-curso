c26d1fc3ae9debf58efad68bbc7c3bcb09c6bc8a
Pablo Gaido <pablo.gaido@unc.edu.ar>
2024-07-08 10:54:35 -0300

    Actualizacion

--- 03-nest-teslo-shop/src/app.module.ts
+++ 03-nest-teslo-shop/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -14,14 +16,18 @@ import { CommonModule } from './common/common.module';
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD,      
       autoLoadEntities: true,
-      synchronize: true
+      synchronize: true,
     }),
 
     ProductsModule,
 
     CommonModule,
+
+    SeedModule,
+
+    FilesModule,
   ],
 })
 export class AppModule {}
